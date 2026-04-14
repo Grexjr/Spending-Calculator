95c5cb00ab3236e910e7af3dffb1041b70e4c392
Grexjr <64386419+Grexjr@users.noreply.github.com>
2026-04-14 17:39:23 -0400

    Swing g UI (#2)
    * SWING BEGINNINGS - BASIC CLASSES AND WRAPPERS 1
    
    CHANGELOG:
    -- Adds Basic Swing classes, including:
    --- Frame, wrapper for JFrame
    --- OptionsPanel, for a panel with button options
    --- ListPanel, for a panel with a text area to display a list of items
    --- TablePanel, for a panel with a JTable to display a table of things
    
    ISSUES:
    N/A
    
    TODO:
    N/A
    
    * SWING BEGINNINGS - BASIC CLASSES AND WRAPPERS 2
    
    CHANGELOG:
    -- Adds buttons to main menu screen that function to swap the screen with another panel
    -- Adds necessary getter models to classes to have them work with the Swing UI
    -- Adds methods to Test Driver to ensure all testing works properly -- visual tester
    
    ISSUES:
    N/A
    
    TODO:
    -- Need to refactor wrappers to not expose the Swing stuff, but instead expose methods that utilize the swing stuff to provide what other classes need
    -- Need to abstract from each panel into either an interface or abstract class that will better allow the SwingUI to work with them
    -- Need to add displayer classes for displaying text
    
    * REFACTOR FOR CLARITY
    
    CHANGELOG:
    -- Adds an abstract panel class that all panels can inherit from to make managing them easier
    -- Adds methods to Frame to better integrate with the app: addPanel and removePanel, rather than resetting the content pane
    -- Renames Frame class to AppFrame for clearer app structure
    -- Adjusts SwingUI to use the new changes in individual classes
    -- Adds better constructors through overloading to OptionsPanel in order to better have labels and specific layouts of button options
    
    ISSUES:
    N/A
    
    TODO:
    -- Need to refactor wrappers to not expose the Swing stuff, but instead expose methods that utilize the swing stuff to provide what other classes need
    -- Need to abstract from each panel into either an interface or abstract class that will better allow the SwingUI to work with them
    -- Need to add displayer classes for displaying text
    
    * RESET
    
    This commit resets the Swing architecture and adds a design document.

---
+++ src/main/java/io/github/grexjr/spendingcalculator/ui/swing/Window.java
@@ -0,0 +1,15 @@
+package io.github.grexjr.spendingcalculator.ui.swing;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    public Window(int width, int height){
+        setSize(width,height);
+
+    }
+
+
+
+
+}
